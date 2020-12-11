7764ebb797819d99be41ce8c0883e9c3bd8c53cf
YKahveciSAU <75043387+YKahveciSAU@users.noreply.github.com>
2020-12-11 03:17:24 +0300

    Uye Olma Ekrani

--- HaberMobil/src/components/Login/SingUp.js
+++ HaberMobil/src/components/Login/SingUp.js
@@ -1,12 +1,63 @@
-import * as React from 'react';
-import {View,Text} from 'react-native'
+import React,{useState,useEffect} from 'react';
+import {View,Text,Dimensions,TextInput,TouchableOpacity} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+const Height=Dimensions.get("window").height;
+const Width=Dimensions.get("window").width;
 function SingUp() {
+    const [userName,setUserName]=useState("")
+    const [userPassword,setUserPassword]=useState("")
+    const [comfirmPassword,setComfirmPassword]=useState("")
+    const [nameField,setNameField]=useState(true);
+    const [passField,setPassField]=useState(true)
+    const [backColor,setBackColor]=useState("#eee")
+    function Gonder(){
+        userName === "" ? setNameField(false) : setNameField(true) ;
+        userPassword === "" ? setPassField(false) : setPassField(true);
+    }
+
+    useEffect(()=>{
+        comfirmPassword===userPassword ? setBackColor("#eee") : setBackColor("#f00");
+    },[comfirmPassword])
+
     return (
-        <View>
-            <Text>
-                bbbb
-            </Text>
-        </View>
+            <View style={{display:"flex",alignItems:"center",justifyContent:"center",backgroundColor:"#fff",height:Height,flex:1}}>
+                <View>
+                    <View style={{display:"flex",alignItems:"center",marginBottom:30}}>
+                        <Icon name={"users"} size={80}/>
+                    </View>
+                    <View style={[{display:"flex",flexDirection:"row",alignItems:"center",margin:3,marginTop:10,padding:10,borderWidth:1,paddingLeft:15,paddingRight:10,backgroundColor:"#eee",borderColor:"#eee",borderRadius:10},nameField ? {borderColor:"#eee"}: {borderColor:"#f00"}]}>
+                        <Icon name={"envelope"} size={24}/>
+                        <TextInput
+                            placeholder="Kullanici Adi"
+                            onChangeText={(name) => setUserName(name)}
+                            style={{marginLeft:10,width:Width/2,fontSize:18,paddingLeft:5}}
+                        />
+                    </View>
+                    <View style={[{display:"flex",flexDirection:"row",alignItems:"center",margin:3,marginTop:10,padding:10,borderWidth:1,paddingLeft:15,paddingRight:10,backgroundColor:"#eee",borderColor:"#eee",borderRadius:10},passField ? {borderColor:"#eee"}: {borderColor:"#f00"}]}>
+                        <Icon name={"lock"} size={30}/>
+                        <TextInput
+                            placeholder="Sifre"
+                            secureTextEntry={true}
+                            onChangeText={(password) => setUserPassword(password)}
+                            style={{marginLeft:10,width:Width/2,fontSize:18,paddingLeft:5}}
+                        />
+                    </View>
+                    <View style={[{display:"flex",flexDirection:"row",alignItems:"center",margin:3,marginTop:10,padding:10,borderWidth:1,paddingLeft:15,paddingRight:10,backgroundColor:"#eee",borderColor:"#eee",borderRadius:10},{borderColor:backColor}]}>
+                        <Icon name={"unlock"} size={30}/>
+                        <TextInput
+                            placeholder="Sifre Dogrula"
+                            secureTextEntry={true}
+                            onChangeText={(pass) => setComfirmPassword(pass)}
+                            style={{marginLeft:10,width:Width/2,fontSize:18,paddingLeft:5}}
+                        />
+                    </View>
+                </View>
+                <TouchableOpacity onPress={()=> Gonder()}>
+                    <Text style={{backgroundColor:"#57b847",margin:3,marginTop:10,padding:10,width:Width/2,borderRadius:10,color:"#fff",fontSize:18,textAlign:"center",fontWeight:"bold"}}>
+                        Kayit Ol
+                    </Text>
+                </TouchableOpacity>
+            </View>
     )
 }
 
